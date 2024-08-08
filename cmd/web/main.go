05f8bd54542b471bad300679566411bb5ba219fc
whoopii <woohyeon88@daolcompany.com>
2024-08-08 20:22:58 +0900

    Installing and setting up a sessions package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/simplifywoopii88/modern_web_applications_with_go/pkg/config"
 	"github.com/simplifywoopii88/modern_web_applications_with_go/pkg/handlers"
 	"github.com/simplifywoopii88/modern_web_applications_with_go/pkg/render"
@@ -14,8 +16,21 @@ const portNumber = ":8080"
 
 
 
+var app config.AppConfig
+var session *scs.SessionManager
 func main(){
-	var app config.AppConfig
+	
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
