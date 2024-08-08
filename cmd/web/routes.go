2758e5870d2299618edd5d0270b6e51ae0728746
simplifywoopii <simplifywoopii@gmail.com>
2024-08-08 17:25:51 +0900

    Developing our own middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,17 +4,22 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/simplifywoopii88/modern_web_applications_with_go/pkg/config"
 	"github.com/simplifywoopii88/modern_web_applications_with_go/pkg/handlers"
 )
 
-func routes(app *config.AppConfig) http.Handler{
+func routes(app *config.AppConfig) http.Handler {
 	// mux := pat.New()
 
 	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/simplifywoopii88/modern_web_applications_with_go
 go 1.21.0
 
 require github.com/go-chi/chi v1.5.5
+
+require github.com/justinas/nosurf v1.1.1 // indirect
